9bdf30278071b3b6ef3d1aed3a65179cbcb9f41a
Marcelin Sigha <marcsigha@gmail.com>
2025-12-17 11:56:28 -0700

    multipage

---
+++ frontend/pages/3_Room.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Rooms")
+
+st.markdown("# Rooms")
+st.sidebar.header("Plotting Demo")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
+
+
+st.button("Re-run")
